8bdc162f67bd8c111fb6359e00292e6645ee2af4
unknown <kolkuzadanok@yandex.com>
2020-04-04 20:03:04 +0100

    adding homework4

---
+++ basic/5/domus5/car_troubleshooter/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace Driver
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public double Skill { get; set; }
+
+    }
+
+    public class Car
+    {
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        public string Driver { get; set; }
+
+        public double CalculateSpeed()
+        {
+            double speed = Driver.Skill * Speed;
+            return speed;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void RaceCars()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            //instanciating cars
+
+            Car lamborgini = new Car();
+            lamborgini.Model = "Lamborgini";
+            lamborgini.Speed = 405;
+            //lamborgini.Driver = // how to assign the driver;
+
+            Car ferrari = new Car();
+            ferrari.Model = "Ferrari";
+            ferrari.Speed = 385;
+
+            Car bugatti = new Car();
+            bugatti.Model = "Bugatti";
+            bugatti.Speed = 407;
+
+            Car ficho = new Car();
+            ficho.Model = "Monster Ficho";
+            ficho.Speed = 421;
+
+            object[] cars = {lamborgini, ferrari, bugatti, ficho };
+
+            //instanciating drivers
+
+            Driver driver01 = new Driver();
+            driver01.Name = "Blasing Jane";
+            driver01.Skill = 1.14;
+
+            Driver driver02 = new Driver();
+            driver02.Name = "Jonny Rocket";
+            driver02.Skill = 0.998;
+
+
+            Driver driver03 = new Driver();
+            driver03.Name = "Lightning Lucy";
+            driver03.Skill = 1.12;
+
+            Driver driver04 = new Driver();
+            driver04.Name = "Trpe Munja";
+            driver04.Skill = 1.15;
+
+            object[] drivers = { driver01, driver02, driver03, driver04 };
+
+            
+            Console.ReadLine();
+            //RaceCars
+
+        }
+
+
+    }
+}
+
+//public double Quotient
+//{
+//    get
+//    {
+//        return Driver.Skill * Speed;
+//    }
+//}
+
+
+// backup
+
+//namespace Driver
+//{
+//    public class Driver
+//    {
+//        public string Name { get; set; }
+//        public double Skill { get; set; }
+
+//    }
+
+//    public class Car
+//    {
+//        public string Model { get; set; }
+//        public double Speed { get; set; }
+//        public string Driver { get; set; }
+
+//        public double CalculateSpeed()
+//        {
+//            double speed = Driver.Skill * Speed;
+//            return speed;
+//        }
+
+//    }
+
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //instanciating cars
+
+//            Car lamborgini = new Car();
+//            lamborgini.Model = "Lamborgini";
+//            lamborgini.Speed = 405;
+//            //lamborgini.Driver = // how to assign the driver;
+
+//            Car ferrari = new Car();
+//            ferrari.Model = "Ferrari";
+//            ferrari.Speed = 385;
+
+//            Car bugatti = new Car();
+//            bugatti.Model = "Bugatti";
+//            bugatti.Speed = 405;
+
+//            Car fikjo = new Car();
+//            fikjo.Model = "Monster Fikjo";
+//            fikjo.Speed = 441;
+
+//            //instanciating drivers
+
+//            Driver driver01 = new Driver();
+//            driver01.Name = "Blasing Jane";
+//            driver01.Skill = 1.14;
+
+//            Driver driver02 = new Driver();
+//            driver02.Name = "Jonny Rocket";
+//            driver02.Skill = 0.998;
+
+
+//            Driver driver03 = new Driver();
+//            driver03.Name = "Lightning Lucy";
+//            driver03.Skill = 1.12;
+
+//            Driver driver04 = new Driver();
+//            driver04.Name = "Trpe Munja";
+//            driver04.Skill = 1.15;
+
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//public double Quotient
+//{
+//    get
+//    {
+//        return Driver.Skill * Speed;
+//    }
+//}
---
+++ basic/5/domus5/dzukche_i.e._dog/dzukche.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace dzukche_i.e._dog
+{
+
+
+    public class Dzukche
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is now eating the prosciuto you`ve bought.");
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{Name} is now playing. Probably hiding your shoes somewhere...");
+        }
+        public void ChaseTail()
+        {
+            Console.WriteLine($"{Name} is now chasing its tail. Oh, no, wait... it actually contemplates on singularity!");
+        }
+
+        public void Activity()
+        {
+            Console.WriteLine($"\n Great, you are now a proud owner of {Name}, a {Color} {Race} ! \n What would you like {Name} to do? \n Please choose one of the three options by entering the respective number");
+            Console.WriteLine($"\n 1. {Name} should EAT. \n 2. {Name} should PLAY. \n 3. {Name} should CHASE ITS TAIL.");
+            int entry = int.Parse(Console.ReadLine());
+            switch (entry)
+            {
+                case 1:
+                    Eat();
+                    break;
+                case 2:
+                    Play();
+                    break;
+                case 3:
+                    ChaseTail();
+                    break;
+                default:
+                    if (entry != 1 ^ entry != 2 ^ entry != 3)
+                    {
+                        Console.WriteLine("Your dog is \"arrested\" by Animal Control! You should be better dog owner or maybe not own a dog at all!");
+                    }
+                    else while (String.IsNullOrEmpty(entry.ToString())) // null/empty input validation! won`t work
+                        {
+                            Console.WriteLine("Please enter value!");
+                            entry = int.Parse(Console.ReadLine());
+
+                        }
+                    break;
+            }
+        }
+        public void DisplayDog()
+        {
+           
+        }
+
+    }
+    class dzukche
+    {
+        static void Main(string[] args)
+        {
+            Dzukche sampleDzukac = new Dzukche();
+
+            Console.WriteLine("Enter the name of your dog");
+            sampleDzukac.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the race of your dog");
+            sampleDzukac.Race = Console.ReadLine();
+
+            Console.WriteLine("Enter the color of your dog");
+            sampleDzukac.Color = Console.ReadLine();
+
+            
+            sampleDzukac.Activity();
+            Console.ReadLine();
+           
+
+        }
+    }
+
+    
+}
